ed1bdd60e94a38586cafcff9c7b8e5df7d1a67e7
roynaldi19 <roynaldi19@gmail.com>
2023-07-09 17:43:19 +0700

    stage 2 complate

--- Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -1,5 +1,52 @@
 package cinema
 
+import kotlin.system.exitProcess
+
 fun main() {
-    // write your code here
+//    // write your code here
+//    val rows = 7
+//    val seatsPerRow = 8
+//
+//    println("Cinema:")
+//    print("  ")
+//    for (seat in 1..seatsPerRow) {
+//        print("$seat ")
+//    }
+//    println()
+//
+//    for (row in 1..rows) {
+//        print("$row ")
+//        for (seat in 1..seatsPerRow) {
+//            print("S ")
+//        }
+//        println()
+//    }
+
+    println("Enter the number of rows:")
+    val rows = readln().toInt()
+    println("Enter the number of seats in each row:")
+    val seats = readln().toInt()
+
+    val totalSeats = rows * seats
+
+    val frontHalfRows: Int
+    val backHalfRows: Int
+
+    if (rows % 2 == 0) {
+        frontHalfRows = rows / 2
+        backHalfRows = rows / 2
+    } else {
+        frontHalfRows = rows / 2
+        backHalfRows = rows - frontHalfRows
+    }
+
+    val profit: Int
+    if (totalSeats <= 60) {
+        profit = totalSeats * 10
+    } else {
+        profit = (frontHalfRows * seats * 10) + (backHalfRows * seats * 8)
+    }
+
+    // Print the total incom
+    println("Total income:$$profit")
 }
